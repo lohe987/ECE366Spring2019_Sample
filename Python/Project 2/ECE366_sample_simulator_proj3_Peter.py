f7d9d0b1c7a7e540ddd6dafb707f45f9f7340d93
flyingpiggs <43591485+flyingpiggs@users.noreply.github.com>
2019-02-19 22:41:22 -0600

    Create ECE366_sample_simulator_proj3_Peter.py

---
+++ Python/Project 2/ECE366_sample_simulator_proj3_Peter.py
@@ -0,0 +1,83 @@
+# Sample simulator for ECE366, spring 2019
+# Instructor: Dr. Wenjing Rao
+# Code written by Peter
+
+# Supported instructions along with some assumptions:
+#   ori: I-type, assuming zero extension
+#   addi: I-type, assuming sign extension, can deal with negative numbers in 2's cmp
+#   sub: R-type, assuming registers containing numbers in 2's cmp
+#   beq: I-type, with the following functionality:
+#       if rs == rt, pc = pc+4+imm<<2
+#       if rs != rt, pc = pc+4
+
+def simulate( instructions, instructionsHex, debugMode ):
+	registers = [0, 0, 0, 0, 0, 0, 0, 0]
+	#memSize = 80 	#in bytes
+	#memory = [0 for i in range( memSize )]
+	programDone = False
+	PC = 0		#program counter 
+	DIC = 0 	#dynamic instruction count
+	fetch = instructions[PC] 
+
+	print( "Starting simulation..." )
+	while ( not( programDone ) ):
+		if ( fetch == "00010000000000001111111111111111" ):
+			programDone = True
+			DIC += 1
+		elif ( fetch[0:6] == "001000" ):	#addi
+			imm = int( fetch[16:32],2 ) if fetch[16] == '0' else -( 65536 - int( fetch[16:32],2 ) ) #range of number for 16 bit unsigned is 0 to 65535
+			if ( debugMode ):
+				print( "PC =" + str(PC*4) + " Instruction: 0x" +  instructionsHex[PC] + " :" + "addi $" + str(int(fetch[6:11],2)) + ",$" + str(int(fetch[11:16],2)) + "," + str(imm) )
+			PC += 1
+			DIC += 1
+			registers[int(fetch[11:16], 2)] = registers[int(fetch[6:11], 2)] + imm
+		elif ( fetch[0:6] == "001101" ):	#ori
+			imm = int( fetch[16:32],2 ) if fetch[16] == '0' else -( 65536 - int( fetch[16:32],2 ) ) #range of number for 16 bit unsigned is 0 to 65535
+			if ( debugMode ):
+				print("PC =" + str(PC*4) + " Instruction: 0x" +  InstructionHex[PC] + " :" + "ori $" + str(int(fetch[6:11],2)) + ",$" + str(int(fetch[11:16],2)) + "," + str(imm) )
+			imm = int( fetch[16:32],2 )	#I'm basically ignoring the fact that this could be a negative number since it we're zero-extending it.
+			PC += 1
+			DIC += 1
+			registers[int(fetch[11:16], 2)] = registers[int(fetch[6:11], 2)] | imm
+		elif ( fetch[0:6] == "000000" ):	#sub
+			if ( debugMode ):
+				print("PC =" + str(PC*4) + " Instruction: 0x" +  instructionsHex[PC] + " :" + "sub $" + str(int(fetch[16:21],2)) + ",$" + str(int(fetch[6:11],2)) + ",$" + str(int(fetch[11:16],2)) )
+			PC += 1
+			DIC += 1
+			register[int(fetch[16:21], 2)] = registers[int(fetch[6:11], 2)] - registers[int(fetch[11:16], 2)]
+		elif ( fetch[0:6] == "000100" ):	#beq
+			if ( debugMode ):
+				print( "PC =" + str(PC*4) + " Instruction: 0x" +  instructionsHex[PC] + " :" + "beq $" + str(int(fetch[6:11],2)) + ",$" + str(int(fetch[11:16],2)) + "," + str(imm) )
+			PC += 1
+			DIC += 1
+			imm = int( fetch[16:32],2 ) if fetch[16] == '0' else -( 65536 - int( fetch[16:32],2 ) )
+			if ( registers[int(fetch[6:11], 2)] == registers[int(fetch[11:16], 2)] ):
+				PC = PC + imm
+		else:
+			print( "Unsupported instruction, ending simulator..." )
+			programDone = True
+
+
+def main():
+	print( "Note: This program assumes that the instructions are in hex." )
+	fileName = input( "Please enter MIPS instruction file name: " )
+	inputFile = open( fileName, "r" )
+	instructions = []	# Declares instructions to be an array
+	instructionsHex = []
+	for line in inputFile:
+		if ( line == "\n" or line[0] =='#' ):              # empty lines and comments ignored
+			continue
+		for index in line:			#This loop will remove any comments. 
+			if ( line[index] == "#" ):
+				line = line[:index] 
+		line = line.replace( '\n', '' )	# Removes new line characters
+		instructionsHex.append( line )
+		line = format( int( line, 16 ), "032b" )	# The int function converts the hex instruction into some numerical value, then format converts it into a binary string 
+		instructions.append( line )
+	inputFile.close()
+	debugMode = True if  int( input( "1 = debug mode         2 = normal execution\n" ) ) == 1 else False
+	simulate( instructions, debugMode )
+
+
+if __name__ == "__main__":
+    main()
